d666368910f15edf47fcf28d3f018717b2cdc177
Daniel Tvorun <danieltvorun@icloud.com>
2022-07-12 14:29:04 +0300

    add random quote API to project

--- TapAndGet/ViewController.swift
+++ TapAndGet/ViewController.swift
@@ -7,10 +7,16 @@
 
 import UIKit
 
+struct RandomText: Decodable {
+    let quote: String
+}
+
 class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var label: UILabel!
     
-    var urlString = ""
+    var imageApiString = ""
+    let textApiString = "https://nodejs-quoteapp.herokuapp.com/quote"
     
     var imageWidth = ""
     var imageHeight = ""
@@ -19,9 +25,22 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupUrlString()
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        executeImageTask()
+        executeTextTask()
+    }
 
-    @IBAction func getButtonPressed(_ sender: UIButton) {
-        guard let url = URL(string: urlString) else { return }
+    @IBAction func getRandomImageButtonPressed(_ sender: UIButton) {
+        executeImageTask()
+    }
+    
+    @IBAction func getRandomTextPressed(_ sender: UIButton) {
+        executeTextTask()
+    }
+    
+    func executeImageTask() {
+        guard let url = URL(string: imageApiString) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
@@ -37,11 +56,34 @@ class ViewController: UIViewController {
         }.resume()
     }
     
+    func executeTextTask() {
+        guard let url = URL(string: textApiString) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            guard let data = data else { return }
+            
+            do {
+                let text = try JSONDecoder().decode(RandomText.self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.label.text = text.quote
+                }
+            } catch {
+                print(error)
+            }
+        }.resume()
+    }
+    
     func setupUrlString() {
         imageWidth = String(format: "%.0f", imageView.bounds.width)
         imageHeight = String(format: "%.0f", imageView.bounds.height)
         
-        urlString = "https://picsum.photos/\(imageWidth)/\(imageHeight)"
+        imageApiString = "https://picsum.photos/\(imageWidth)/\(imageHeight)"
     }
 }
 
